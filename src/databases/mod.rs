59fa3ab03bfd9e0324035fd3632ab1df52b1cb04
Andrew Wheeler <genusistimelord@gmail.com>
2022-02-23 11:43:04 -0500

    updating to add Doc features

--- src/databases/mod.rs
+++ src/databases/mod.rs
@@ -1,14 +1,20 @@
 #[cfg(feature = "mysql")]
+#[doc(hidden)]
 mod mysql;
 #[cfg(feature = "mysql")]
+#[cfg_attr(docsrs, doc(cfg(feature = "mysql")))]
 pub use mysql::*;
 
 #[cfg(feature = "postgres")]
+#[doc(hidden)]
 mod postgres;
 #[cfg(feature = "postgres")]
+#[cfg_attr(docsrs, doc(cfg(feature = "postgres")))]
 pub use postgres::*;
 
 #[cfg(feature = "sqlite")]
+#[doc(hidden)]
 mod sqlite;
 #[cfg(feature = "sqlite")]
+#[cfg_attr(docsrs, doc(cfg(feature = "sqlite")))]
 pub use sqlite::*;
