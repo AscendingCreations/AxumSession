455d51ff4b04e15e2c55122e9c8731ed3fa65793
11tuvork28 <zofi@xamh.de>
2022-07-22 13:51:43 +0200

    Added missing error variant

--- src/errors.rs
+++ src/errors.rs
@@ -18,4 +18,6 @@ pub enum SessionError {
     GenericSelectError,
     #[error("Generic Database create error")]
     GenericCreateError,
+    #[error("Generic Database delete error")]
+    GenericDeleteError,
 }
