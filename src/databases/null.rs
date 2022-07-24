18b885e1e19c832c84e345f70de4388158d64b7e
Andrew Wheeler <genusistimelord@gmail.com>
2022-07-24 15:18:17 -0400

    fixed clippy issues

--- src/databases/null.rs
+++ src/databases/null.rs
@@ -1,6 +1,5 @@
 use crate::{AxumDatabasePool, SessionError};
 use async_trait::async_trait;
-use chrono::Utc;
 
 /// Null Pool type for AxumDatabasePool.
 /// Use this when you do not want to load any database.
@@ -8,37 +7,37 @@ pub struct AxumNullPool;
 
 #[async_trait]
 impl AxumDatabasePool for AxumNullPool {
-    async fn migrate(&self, table_name: &str) -> Result<(), SessionError> {
+    async fn migrate(&self, _table_name: &str) -> Result<(), SessionError> {
         Ok(())
     }
 
-    async fn delete_by_expiry(&self, table_name: &str) -> Result<(), SessionError> {
+    async fn delete_by_expiry(&self, _table_name: &str) -> Result<(), SessionError> {
         Ok(())
     }
 
-    async fn count(&self, table_name: &str) -> Result<i64, SessionError> {
+    async fn count(&self, _table_name: &str) -> Result<i64, SessionError> {
         return Ok(0);
     }
 
     async fn store(
         &self,
-        id: &str,
-        session: &str,
-        expires: i64,
-        table_name: &str,
+        _id: &str,
+        _session: &str,
+        _expires: i64,
+        _table_name: &str,
     ) -> Result<(), SessionError> {
         Ok(())
     }
 
-    async fn load(&self, id: &str, table_name: &str) -> Result<Option<String>, SessionError> {
+    async fn load(&self, _id: &str, _table_name: &str) -> Result<Option<String>, SessionError> {
         Ok(None)
     }
 
-    async fn delete_one_by_id(&self, id: &str, table_name: &str) -> Result<(), SessionError> {
+    async fn delete_one_by_id(&self, _id: &str, _table_name: &str) -> Result<(), SessionError> {
         Ok(())
     }
 
-    async fn delete_all(&self, table_name: &str) -> Result<(), SessionError> {
+    async fn delete_all(&self, _table_name: &str) -> Result<(), SessionError> {
         Ok(())
     }
 }
