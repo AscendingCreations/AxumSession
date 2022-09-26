d28aa7337798171f3c300a4ba37ac3246ec8c439
Andrew Wheeler(Genusis) <lordsatin@hotmail.com>
2022-09-26 16:49:54 -0400

    unwrap_or then calculated Option

--- src/databases/mysql.rs
+++ src/databases/mysql.rs
@@ -122,7 +122,7 @@ impl AxumDatabasePool for AxumMySqlPool {
         .fetch_optional(&self.pool)
         .await?;
 
-        result.map(|(count,)| count > 0).ok()
+        Ok(result.unwrap_or(0) > 0)
     }
 
     async fn delete_all(&self, table_name: &str) -> Result<(), SessionError> {
