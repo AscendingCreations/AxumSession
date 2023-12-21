a311c7e41aa6b2b9e8687f9a63ca7f50fef9a41d
Ch. König <me@kurabloodlust.eu>
2023-12-22 00:19:21 +0100

    Add support for table names in Redis operations
    This update adds support for table names in Redis and Redis cluster operations. With this improvement, users can count, load, delete and check existence of keys matching the provided table name, along with general functions. An additional Redis tools module has been introduced to support key scanning.

---
+++ src/databases/redis_tools.rs
@@ -0,0 +1,38 @@
+use redis::aio::ConnectionLike;
+use redis_pool::connection::RedisPoolConnection;
+
+use crate::SessionError;
+
+pub async fn scan_keys<C>(
+    con: &mut RedisPoolConnection<C>,
+    pattern: &str,
+) -> Result<Vec<String>, SessionError>
+where
+    C: ConnectionLike + Send + Sync + 'static,
+{
+    // SCAN works like KEYS but it is safe to use in production.
+    // Instead of blocking the server, it will only return a small
+    // amount of keys per iteration.
+    // https://redis.io/commands/scan
+
+    let mut keys: Vec<String> = Vec::new();
+    let mut cursor: i32 = 0;
+
+    loop {
+        let (new_cursor, new_keys): (i32, Vec<String>) = redis::cmd("SCAN")
+            .arg(cursor)
+            .arg("MATCH")
+            .arg(pattern)
+            .query_async(con)
+            .await?;
+
+        keys.extend(new_keys);
+
+        cursor = new_cursor;
+        if cursor == 0 {
+            break;
+        }
+    }
+
+    Ok(keys)
+}
